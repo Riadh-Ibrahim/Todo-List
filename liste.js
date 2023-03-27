f0b50772c227536c13360dfa9d42afb88c2af565
Riadh-Ibrahim <110717872+Riadh-Ibrahim@users.noreply.github.com>
2023-03-27 13:59:19 +0200

    Delete liste.js

--- liste.js
+++
@@ -1,8 +0,0 @@
-const liste=document.querySelector(".liste");
-
-Object.values(liste.children).forEach(child=>{
-    child.addEventListener("click",()=>{
-    const couleuralea="#"+Math.floor(Math.random()*16777215).toString(16) ;
-    child.style.color=couleuralea;   
-});
-});
